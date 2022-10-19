57b79d902a3cb01b6e8edf874265e660f32ba2c6
Manuel Šarić <manujelko@gmail.com>
2022-10-19 14:39:26 +0200

    Create authentication and post request

---
+++ todo/auth.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from . import models
+
+
+class CreateUser(BaseModel):
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str
+    password: str
+
+
+app = FastAPI()
+
+
+@app.post("/create/user")
+async def create_new_user(create_user: CreateUser):
+    create_user_model = models.Users()
+    create_user_model.email = create_user.email
+    create_user_model.username = create_user.username
+    create_user_model.first_name = create_user.first_name
+    create_user_model.last_name = create_user.last_name
+    create_user_model.hashed_password = create_user.password
+    create_user_model.is_active = True
+
+    return create_user_model
